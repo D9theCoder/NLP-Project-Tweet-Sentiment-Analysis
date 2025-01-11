f6c97979566e1888aa77bea890912a28c4282947
kevin <kevinpurnomo06@gmail.com>
2025-01-11 23:06:10 +0700

    p

--- app.py
+++ app.py
@@ -24,7 +24,14 @@ all_stopwords = [w for w in all_stopwords if w not in ['no', 'not']]
 # Load the model and tokenizer
 @st.cache_resource
 def load_artifacts():
-    model = load_model('bilstm.h5', compile = True)
+    try:
+        model = load_model('bilstm.h5', compile=True)
+    except ValueError as e:
+        if 'time_major' in str(e):
+            # Custom load logic for newer TF versions
+            model = load_model('bilstm.h5', compile=True, 
+                             custom_objects={'time_major': False})
+    
     with open('tokenizer.pkl', 'rb') as file:
         tokenizer = pickle.load(file)
     return model, tokenizer
